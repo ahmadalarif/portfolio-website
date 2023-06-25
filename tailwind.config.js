56b8cadfbd57b941471f52112f92743ed51caf65
ahmadalarif <alarif3@gmil.com>
2023-06-25 17:12:23 +0300

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        BPdotsUnicaseSquareLight: 'BPdotsUnicaseSquareLight',
+        BPdotsUnicase: 'BPdotsUnicase',
       }
     },
   },
